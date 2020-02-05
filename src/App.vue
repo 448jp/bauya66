cbcd4fe70a66c7e9d4430658115c5829c6e3be82
OKI Yoshiya <1371326+448jp@users.noreply.github.com>
2020-02-05 18:31:29 +0900

    demo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
   <div id="app">
+    <transition name="fade">
+      <h1 v-if="isClicked" class="heading1">BAU-YA</h1>
+    </transition>
+    <button type="button" @click="onClick">切り替え</button>
+    <ul class="list">
+      <TheCreator
+        v-for="creator in creators"
+        :key="creator.name"
+        :name="creator.name"
+        :age="creator.age"
+        :job="creator.job"
+      />
+    </ul>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -8,7 +21,63 @@
   </div>
 </template>
 
-<style lang="scss">
+<script>
+import TheCreator from "@/components/TheCreator";
+
+export default {
+  components: {
+    TheCreator
+  },
+  data() {
+    return {
+      isClicked: false,
+      creators: [
+        {
+          name: "沖 良矢",
+          age: 38,
+          job: "インタラクションデザイナー"
+        },
+        {
+          name: "生明 義秀",
+          age: 52,
+          job: "デザイナー"
+        },
+        {
+          name: "宮田 優希",
+          age: 33,
+          job: "Webデザイナー"
+        }
+      ]
+    };
+  },
+  methods: {
+    onClick() {
+      this.isClicked = !this.isClicked;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.fade-enter-active {
+  transition: opacity 1s ease-out;
+}
+
+.fade-enter {
+  opacity: 0;
+}
+
+.heading1 {
+  font-size: 100px;
+  color: red;
+}
+
+.list {
+  display: flex;
+  justify-content: center;
+  list-style: none;
+}
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/TheCreator.vue
@@ -0,0 +1,51 @@
+<template>
+  <li class="creator">
+    <p class="creator_name">
+      {{ name }}
+    </p>
+    <p class="creator_age">
+      {{ age }}
+    </p>
+    <p class="creator_job">
+      {{ job }}
+    </p>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    age: Number,
+    job: String
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.creator {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 200px;
+  height: 240px;
+  margin-right: 20px;
+  border: 1px solid gray;
+  border-radius: 8px;
+  background-color: cornsilk;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.4);
+
+  &_name {
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  &_job {
+    font-size: 12px;
+  }
+
+  &_age {
+    color: green;
+  }
+}
+</style>
